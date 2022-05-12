ad59903992fbc65a99438d46b1eff3eb45245320
guttidani <61143051+guttidani@users.noreply.github.com>
2022-05-12 16:48:34 +0200

    Create Auto.java

---
+++ Feladat13/src/Modell/Auto.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modell;
+
+/**
+ *
+ * @author Dani
+ */
+public class Auto {
+    private String Szin;
+    private String Marka;
+
+    public String getSzin() {
+        return Szin;
+    }
+
+    public String getMarka() {
+        return Marka;
+    }
+
+    public Auto() {
+        this.Marka = "VW";
+        this.Szin = "fehér";
+    }
+    
+}
