7461e7edfc94b442410593981e6c0a93fbbc2e00
Dominik Kornél Czuth <cutdomi@gmail.com>
2022-05-14 22:02:04 +0200

    Gombok

--- Feladat13/src/Main/Feladat13.java
+++ Feladat13/src/Main/Feladat13.java
@@ -3,22 +3,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Main;
+
+
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
-/**
- *
- * @author Dani
- */
-public class Feladat13 extends Application{
-    public void start (Stage primaryStage){
-         primaryStage.setTitle("Beadandó");
-         VBox gridBox = new VBox();
-         Scene mainScene = new Scene(gridBox,900,600);
-         primaryStage.setScene(mainScene);
-         primaryStage.show();
-            
+
+public class Feladat13 extends Application {
+
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Beadandó");
+        Button OKbutton = new Button();
+        Button Closebutton = new Button();
+        OKbutton.setText("OK");
+        Closebutton.setText("Close");
+        HBox horiBox = new HBox(OKbutton,Closebutton);
+        VBox gridBox = new VBox(horiBox);
+        //OKbutton.setTranslateX(500);  
+        //OKbutton.setTranslateY(250);
+        
+ 
+        
+        Scene mainScene = new Scene(gridBox, 900, 600);
+        primaryStage.setScene(mainScene);
+        primaryStage.show();
+
     }
+    public static void main(String args[]){
+      launch(args);
+   }
 }
