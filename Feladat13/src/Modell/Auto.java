ae897351f3ab723ff953e3c3b7974af39c53df1d
guttidani <61143051+guttidani@users.noreply.github.com>
2022-05-14 21:12:53 +0200

    Update Auto.java

--- Feladat13/src/Modell/Auto.java
+++ Feladat13/src/Modell/Auto.java
@@ -24,5 +24,23 @@ public class Auto {
         this.Marka = "VW";
         this.Szin = "Fehér";
     }
+
+    public void setSzin(String Szin) {
+        this.Szin = Szin;
+    }
+
+    public void setMarka(String Marka) {
+        this.Marka = Marka;
+    }
+    
+    public Auto(String Szin) {
+        this.Marka = "VW";
+        this.Szin = Szin;
+    }
+    
+    public Auto(String Szin, String Marka) {
+        this.Marka = Marka;
+        this.Szin = Szin;
+    }
     
 }
